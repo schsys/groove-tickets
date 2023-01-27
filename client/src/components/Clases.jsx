adbed9f52fe3fc65f46916f0e9da1846a5281062
Angel Zabaleta Torrez <angelzabaleta08@hotmail.com>
2023-01-27 18:48:24 -0500

    cambio de nombre de los componentes a inglés y cambioe sne la carpeta public archivo index

--- client/src/components/Clases.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Clases = () => {
-  return (
-    <div>Clases</div>
-  )
-}
-
-export default Clases
--- client/src/components/Tienda.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Tienda = () => {
-  return (
-    <div>Tienda</div>
-  )
-}
-
-export default Tienda
