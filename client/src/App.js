7ddf8ed760ed83935ccb6ba50a25ed1a258dd3f3
Diego Llaya <goya0310@gmail.com>
2023-01-27 13:27:49 -0300

    cambios en App y agrego carpeta recursos visuales

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { Route } from "react-router-dom";
+import Funciones from "./components/Funciones";
+import NavBar from "./components/NavBar";
+import Tienda from "./components/Tienda";
+import Clases from "./components/Clases";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <NavBar />
+      <Route exact path={"/"} component={Funciones} />
+      <Route exact path={"/tienda"} component={Tienda} />
+      <Route exact path={"/clases"} component={Clases} />
+    </React.Fragment>
   );
 }
 
