a68769bb995f4a5dedeb9545edb0752c24ced91d
nicobonder <nicobonder@hotmail.com>
2023-01-28 10:45:44 -0300

    folders ok

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,6 @@ import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import store from "../src/redux/store/index.js";
-import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
