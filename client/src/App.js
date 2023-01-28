2e26b050212f8560087b8d4280d155f1bf83dd1e
nicobonder <nicobonder@hotmail.com>
2023-01-27 21:52:28 -0300

    creating Landing Page

--- client/src/App.js
+++ client/src/App.js
@@ -5,12 +5,14 @@ import Shows from "./components/Shows";
 import NavBar from "./components/NavBar";
 import Shop from "./components/Shop";
 import Lessons from "./components/Lessons";
+import LandingPage from "./components/LandingPage/LandingPage"
 
 function App() {
   return (
     <React.Fragment>
       <NavBar />
-      <Route exact path={"/"} component={Shows} />
+      <Route exact path={"/"} component={LandingPage} />      
+      <Route exact path={"/shows"} component={Shows} />
       <Route exact path={"/Shop"} component={Shop} />
       <Route exact path={"/Lessons"} component={Lessons} />
     </React.Fragment>
