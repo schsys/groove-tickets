9b4cc221b1bf17de42f29437ff4aa326fc0e8162
JIM90mdp <juanignaciomascarenhas@gmail.com>
2023-01-27 22:26:02 -0300

    package.json, browserRouter en index.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,23 @@
 import "./App.css";
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Shows from "./components/Shows";
 import NavBar from "./components/NavBar";
 import Shop from "./components/Shop";
 import Lessons from "./components/Lessons";
-import LandingPage from "./components/LandingPage/LandingPage"
+import LandingPage from "./components/LandingPage/LandingPage";
 
 function App() {
   return (
-    <React.Fragment>
+    <div className="App">
       <NavBar />
-      <Route exact path={"/"} component={LandingPage} />      
-      <Route exact path={"/shows"} component={Shows} />
-      <Route exact path={"/Shop"} component={Shop} />
-      <Route exact path={"/Lessons"} component={Lessons} />
-
-    </React.Fragment>
+      <Switch>
+        <Route exact path={"/"} component={LandingPage} />
+        <Route exact path={"/shows"} component={Shows} />
+        <Route exact path={"/Shop"} component={Shop} />
+        <Route exact path={"/Lessons"} component={Lessons} />
+      </Switch>
+    </div>
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -4,15 +4,19 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 import store from "../src/redux/store/index.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
 
 reportWebVitals();
+
