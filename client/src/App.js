7d407baff6f6899d9184becbb597873786d5fc6a
goya0310 <108840268+goya0310@users.noreply.github.com>
2023-01-29 14:54:27 -0300

    landing sin navbar (#11)

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,18 @@
 import "./App.css";
 import React from "react";
-
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, useLocation } from "react-router-dom";
 import Shows from "./components/Shows/Shows";
 import NavBar from "./components/NavBar/NavBar";
 import Shop from "./components/Shop/Shop";
 import Lessons from "./components/Lessons/Lessons";
 
-import LandingPage from "./components/LandingPage/LandingPage"
-
+import LandingPage from "./components/LandingPage/LandingPage";
 
 function App() {
+  const { pathname } = useLocation();
   return (
     <div className="App">
+      {pathname !== "/" ? <NavBar /> : null}
       <NavBar />
       <Switch>
         <Route exact path={"/"} component={LandingPage} />
