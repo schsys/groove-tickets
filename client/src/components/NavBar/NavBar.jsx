a139b9e2cb44de4f5785e6961f6c09e7120b5006
nicobonder <nicobonder@hotmail.com>
2023-01-31 10:03:13 -0300

    added fontawesome in index.html

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,44 +1,60 @@
 import Logo from "./LogoYazz.png";
-import React, {useState} from "react";
-import "./Navbar.css"
+import React, { useState } from "react";
+import "./Navbar.css";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
-    const [isOpen, setIsOpen] = useState(false)
-    const [loggedIn, setLoggedIn] = useState(false);
-    const [username, setUsername] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [username, setUsername] = useState("");
 
-    return(
-        <div className="navbar">
-            <div className="nav_logo">
-              <Link to="/"><img className="navbar_logo_img" src={Logo} alt="" /></Link>
-            </div>
-            <div className={`nav_items ${isOpen && "open"}`}>
-              <Link to={"/shows"} className="navbar_menu_link">SHOWS</Link>
-              <Link to={"/shop"} className="navbar_menu_link">TIENDA</Link>
-              <Link to={"/lessons"} className="navbar_menu_link">CLASES</Link>
-            </div>
-            <div className={`nav_toggle ${isOpen && "open"}`} onClick={ () => setIsOpen(!isOpen)} >
-                <span></span>
-                <span></span>
-                <span></span>
-            </div>
+  return (
+    <div className="navbar">
+      <div className="nav_logo">
+        <Link to="/">
+          <img className="navbar_logo_img" src={Logo} alt="" />
+        </Link>
+      </div>
+      <div className={`nav_items ${isOpen && "open"}`}>
+        <Link to={"/shows"} className="navbar_menu_link">
+          SHOWS
+        </Link>
+        {/* <Link to={"/shop"} className="navbar_menu_link">TIENDA</Link>
+              <Link to={"/lessons"} className="navbar_menu_link">CLASES</Link> */}
+      </div>
+      <div
+        className={`nav_toggle ${isOpen && "open"}`}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
 
-            <div className="nav_right">
-          <div>
-            {loggedIn ? (
-              <div className="nav_username">{username}</div>
-            ) : (
+      <div className="nav_right">
+        <div>
+          {loggedIn ? (
+            <div className="nav_username">{username}</div>
+          ) : (
+            <div className="nav_btn_logged">
               <div className="nav_login_btns">
-                 <Link to={"/micuenta"} className="navbar_menu_link">MI CUENTA</Link>
+                <Link to={"/micuenta"} className="navbar_menu_link">
+                  MI CUENTA
+                </Link>
               </div>
-            )}
-          </div>
+              <div className="nav_cart_btn">
+                <Link to={"/carrito"} className="navbar_menu_link">
+                  <i id="cart-icon" className="fa-solid fa-cart-shopping" ></i>
+                </Link>
+              </div>
+            </div>
+          )}
         </div>
-        </div>
-    )
-}
-export default Navbar
+      </div>
+    </div>
+  );
+};
+export default Navbar;
 
 /*import Logo from "./LogoYazz.png";
 import React, { useEffect, useState } from "react";
