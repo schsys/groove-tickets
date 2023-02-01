d87c3ef07b26b80699a4b058eca92bccb4ff4745
nicobonder <nicobonder@hotmail.com>
2023-01-31 23:40:22 -0300

    MUI installted. Footer started

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Box, Container, Stack } from '@mui/material';
+import { Link } from '@mui/material';
+
+export default function Footer() {
+  return (
+    <div>
+      <Container maxWidth="sm">
+        <Box>
+          <Link>
+          
+          </Link>
+        </Box>
+        <Stack>
+          
+        </Stack>
+      </Container>
+    </div>
+  )
+}
