a6f292ad1b42f731f1ba9b291e764cbd6c3dfb83
Angel Zabaleta Torrez <angelzabaleta08@hotmail.com>
2023-02-02 07:53:19 -0500

    Componente ProductDetails desarrollado en un 70% correción del salto de página de la foto

--- client/src/components/ProductDetails/ProductDetails.jsx
+++ client/src/components/ProductDetails/ProductDetails.jsx
@@ -19,32 +19,34 @@ const props = {
 export default function ProductDetails() {
   return (
     <div className="container-details">
-      <div className="product-container">
-        <h2>{props.name}</h2>
-        <span>
-          {Array.from({ length: props.rating }, (_, i) => (
-            <i key={i} class="fa fa-star"></i>
-          ))}
-        </span>
-        <p>
-          <i class="fas fa-calendar"></i> {props.date}
-        </p>
-        <p>
-          <i class="fas fa-clock"></i> {props.time} horas
+      <div className="global-container">
+        <div className="product-container">
+          <h2>{props.name}</h2>
+          <span>
+            {Array.from({ length: props.rating }, (_, i) => (
+              <i key={i} class="fa fa-star"></i>
+            ))}
+          </span>
           <p>
-            <i class="fas fa-music"></i> Músicos: {props.musicians}
+            <i class="fas fa-calendar"></i> {props.date}
           </p>
           <p>
-            <i class="fas fa-tag"></i> {props.category}
+            <i class="fas fa-clock"></i> {props.time} horas
+            <p>
+              <i class="fas fa-music"></i> Músicos: {props.musicians}
+            </p>
+            <p>
+              <i class="fas fa-tag"></i> {props.category}
+            </p>
+            <p>
+              <i class="fas fa-map-marker-alt"></i> Ubicación: {props.location}
+            </p>
           </p>
-          <p>
-            <i class="fas fa-map-marker-alt"></i> Ubicación: {props.location}
-          </p>
-        </p>
-        <h2>Precio: ${props.price}</h2>
-      </div>
-      <div className="image-container">
-        <img src={props.img} alt="product" className="product-image" />
+          <h2>Precio: ${props.price}</h2>
+        </div>
+        <div className="image-container">
+          <img src={props.img} alt="product" className="product-image" />
+        </div>
       </div>
       <button className="product-button">Comprar</button>
       <div className="product-info">
