0bb4b49043b5b88897887dd508b5fd41dbc385c0
Angel Zabaleta Torrez <angelzabaleta08@hotmail.com>
2023-02-04 07:09:33 -0500

    componente ProductDetails parcialmente terminado

--- client/src/components/ProductDetails/ProductDetails.jsx
+++ client/src/components/ProductDetails/ProductDetails.jsx
@@ -9,6 +9,7 @@ import { useParams } from "react-router-dom";
 export default function ProductDetails() {
   const { id } = useParams();
   const product = useSelector((state) => state.product);
+
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -32,37 +33,71 @@ export default function ProductDetails() {
             <i className="fa fa-star"></i>
             <i className="fa fa-star"></i>
           </span>
-          <p>
-            <i className="fas fa-calendar"></i> {formattedDate}
-          </p>
-          <p>
-            <i className="fas fa-clock"></i> {product.StartTime} horas
-            {/*  <p>
-              <i className="fas fa-music"></i> Músicos: {product.Artist.Name}
-            </p> */}
-            {/*   <p>
-              <i className="fas fa-tag"></i> {product.Categories.name}
-            </p>
+
+          <>
             <p>
-              <i className="fas fa-map-marker-alt"></i> Ubicación:{" "}
-              {product.Location.Address}
-            </p> */}
-          </p>
-          <h2>Precio: ${product.Price}</h2>
+              <i className="fas fa-calendar"></i> {formattedDate}
+            </p>
+          </>
+
+          <>
+            <i className="fas fa-clock"></i> {product.StartTime} horas
+          </>
+
+          <>
+            {product.Artist && Object.keys(product.Artist).length > 0 ? (
+              <p>
+                <i className="fas fa-music"></i> Músico: {product.Artist.Name}
+              </p>
+            ) : (
+              <p>Músico no disponible</p>
+            )}
+          </>
+
+          <>
+            {product.Categories && product.Categories.length > 0 ? (
+              <p>
+                {" "}
+                <i className="fas fa-tag"></i> {product.Categories[0].Name}
+              </p>
+            ) : (
+              <p>No hay categorías disponibles</p>
+            )}
+          </>
+          <>
+            {product.Location && Object.keys(product.Location).length > 0 ? (
+              <p>
+                <i className="fas fa-map-marker-alt"></i> Ubicación:{" "}
+                {product.Location.Name}
+                {" -"} {product.Location.Address}
+              </p>
+            ) : (
+              <p>No hay ubicación disponible</p>
+            )}
+          </>
+          <>
+            <h2>Precio: ${product.Price}</h2>
+          </>
         </div>
-        {/*  <div className="image-container">
-          <img
-            src={product.Photos[0].Path}
-            alt="product"
-            className="product-image"
-          />
-        </div> */}
+
+        <div className="image-container">
+          {product.Photos && product.Photos.length > 0 ? (
+            <img
+              src={product.Photos[0].Path}
+              alt="product"
+              className="product-image"
+            />
+          ) : (
+            <p>No hay imágenes disponibles</p>
+          )}
+        </div>
       </div>
       <button className="product-button">Comprar</button>
       <div className="product-info">
         <h4>Descripción:</h4>
         <p>{product.Description}</p>
       </div>
+      <Footer />
     </div>
   );
 }
