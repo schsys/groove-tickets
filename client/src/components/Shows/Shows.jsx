cc2b8ce03536edc29fb701104409c22559a83c40
goya0310 <108840268+goya0310@users.noreply.github.com>
2023-02-06 15:58:51 -0300

    filtros combinados terminados y arreglos esteticos pagina shows (#41)
    * cambio banner shows
    
    * filtros combinados terminados y arreglos esteticos pagina shows

--- client/src/components/Shows/Shows.jsx
+++ client/src/components/Shows/Shows.jsx
@@ -5,24 +5,73 @@ import Pagination, { productIndex } from "../Pagination/Pagination";
 import { useDispatch, useSelector } from "react-redux";
 import { getProducts } from "../../redux/actions";
 import { Link } from "react-router-dom";
-import {filterProductsByDate} from "../../redux/actions"
+import { filterProducts } from "../../redux/actions";
 
 const Shows = () => {
-  const [input, setInput] = useState("");
+  const [selectedDay, setSelectedDay] = useState("");
+  const [selectedCategoryId, setSelectedCategoryId] = useState("");
   const products = useSelector((state) => state.products);
   const dispatch = useDispatch();
 
-  // PARA FILTRADO POR FECHA
-  const handleOrderByName = (day) => {
-    dispatch(filterProductsByDate(day));
-    setInput(day);
-    setCurrentPage(1);
+  const formattedDate = (StartDate) => {
+    const date = new Date(StartDate + "T00:00:00");
+    const options = { weekday: "long", day: "numeric", month: "numeric" };
+    let formattedDate = date.toLocaleDateString("es-ES", options);
+    formattedDate = formattedDate.replace("/", ".");
+    formattedDate = formattedDate.replace(",", "");
+
+    return formattedDate;
   };
+
+  function countCategories(arr, name) {
+    let count = 0;
+
+    arr.forEach((e) => {
+      e.Categories.forEach((category) => {
+        if (category.name === name) {
+          count++;
+        }
+      });
+    });
+
+    return count;
+  }
+  const categories = [];
+  products.forEach((event) => {
+    event.Categories.forEach((category) => {
+      categories.push({
+        id: category.id,
+        name: category.name,
+      });
+    });
+  });
+
+  const uniqueCategories = [
+    ...new Set(categories.map((category) => category.name)),
+  ].map((name) => {
+    return {
+      id: categories.find((category) => category.name === name).id,
+      name: name,
+    };
+  });
+
+  const handleDayChange = (day) => {
+    setSelectedDay(day);
+  };
+
+  const handleCategoryChange = (categoryId) => {
+    setSelectedCategoryId(categoryId);
+  };
+
   //
 
   useEffect(() => {
     dispatch(getProducts());
   }, [dispatch]);
+
+  useEffect(() => {
+    dispatch(filterProducts(selectedDay, selectedCategoryId));
+  }, [selectedDay, selectedCategoryId, dispatch]);
 
   const [currentPage, setCurrentPage] = useState(1);
 
@@ -35,17 +84,6 @@ const Shows = () => {
   };
 
   const { lastProduct, firstProduct } = productIndex(currentPage, 6);
-
-  const formattedDate = (StartDate) => {
-    const date = new Date(StartDate);
-    const options = { weekday: "long", day: "numeric", month: "numeric" };
-    let formattedDate = date.toLocaleDateString("es-ES", options);
-    formattedDate = formattedDate.replace("/", ".");
-    formattedDate = formattedDate.replace(",", "");
-
-    return formattedDate;
-  };
-
   return (
     <div className="shows__background-container">
       <img src={banner} alt="banner shows" className="shows__banner-img" />
@@ -54,106 +92,171 @@ const Shows = () => {
 
       <div className="shows__filter-textcontainer">
         <div className="shows__filter-text">
-          <h4>PRÓXIMAS FUNCIONES</h4>
+          <h4>
+            {selectedDay === "" ? (
+              "PRÓXIMAS FUNCIONES"
+            ) : (
+              <button
+                onClick={() => {
+                  handleDayChange(selectedDay !== "" ? "" : selectedDay);
+                }}
+              >
+                QUITAR FILTRO (X)
+              </button>
+            )}
+          </h4>
         </div>
       </div>
       <div className="shows__filter-datescontainer">
-        <div className="shows__filter-box">
-          <button 
-          onClick={() => {
-            handleOrderByName(1)
-          }}
-          >1 DÍA</button>
-          <button onClick={() => {
-            handleOrderByName(7)
-          }}>7 DÍAS</button>
-          <button onClick={() => {
-            handleOrderByName(15)
-          }}>15 DÍAS</button>
+        <div className={"shows__filter-box"}>
+          <button
+            style={selectedDay === 1 ? { color: `var(--color-yellow)` } : {}}
+            onClick={() => {
+              handleDayChange(1);
+            }}
+          >
+            1 DÍA
+          </button>
+          <button
+            style={selectedDay === 7 ? { color: `var(--color-yellow)` } : {}}
+            onClick={() => {
+              handleDayChange(7);
+            }}
+          >
+            7 DÍAS
+          </button>
+          <button
+            style={selectedDay === 30 ? { color: `var(--color-yellow)` } : {}}
+            onClick={() => {
+              handleDayChange(30);
+            }}
+          >
+            15 DÍAS
+          </button>
         </div>
       </div>
 
       {/*FIN FILTRADO  POR FECHAS*/}
 
-      <div className="shows__cards-container">
-        {Array.isArray(products) === false ? (
-          <div className="shows__cards-box1">
-            <img
-              src={products.Photos[0].Path}
-              alt="imagen show1"
-              className="shows__cards-show1"
-            />
-            <div className="shows__cards-textContainer">
-              <h1 className="shows__cards-texth1">{products.name}</h1>
-              <h2 className="shows__cards-texth2">
-                {formattedDate(products.StartDate).replace(/^\w/, (c) =>
-                  c.toUpperCase()
-                )}
-              </h2>
-              <h3 className="shows__cards-texth3">
-                {products.StartTime.slice(0, 2)}
-              </h3>
-              <Link
-                to={`product/${products.id}`}
-                className="shows_cards-linkInfo"
-              >
-                +
-              </Link>
-            </div>
-          </div>
-        ) : (
-          products?.slice(firstProduct, lastProduct).map((product) => {
-            return (
-              <div className="shows__cards-box1">
-                <img
-                  // src={imagen}
-                  src={product.Photos[0].Path}
-                  alt="imagen show1"
-                  className="shows__cards-show1"
-                />
-                <div className="shows__cards-textContainer">
-                  <h1 className="shows__cards-texth1">{product.name}</h1>
-                  <h2 className="shows__cards-texth2">
-                    {formattedDate(product.StartDate).replace(/^\w/, (c) =>
-                      c.toUpperCase()
-                    )}
-                  </h2>
-                  <h3 className="shows__cards-texth3">
-                    {product.StartTime.slice(0, 2)} Horas
-                  </h3>
-                  <Link
-                    to={`product/${product.id}`}
-                    className="shows_cards-linkInfo"
-                  >
-                    +
-                  </Link>
-                </div>
+      {products.length ? (
+        <div className="shows__cards-container">
+          {Array.isArray(products) === false ? (
+            <div className="shows__cards-box1">
+              <img
+                src={products.Photos[0].Path}
+                alt="imagen show1"
+                className="shows__cards-show1"
+              />
+              <div className="shows__cards-textContainer">
+                <h1 className="shows__cards-texth1">{products.name}</h1>
+                <h2 className="shows__cards-texth2">
+                  {formattedDate(products.StartDate).replace(/^\w/, (c) =>
+                    c.toUpperCase()
+                  )}
+                </h2>
+                <h3 className="shows__cards-texth3">
+                  {products.StartTime.slice(0, 2)}
+                </h3>
+                <Link
+                  to={`product/${products.id}`}
+                  className="shows_cards-linkInfo"
+                >
+                  +
+                </Link>
               </div>
-            );
-          })
-        )}
-      </div>
-
+            </div>
+          ) : (
+            products
+              ?.sort(
+                (a, b) =>
+                  new Date(a.StartDate + "T00:00:00") -
+                  new Date(b.StartDate + "T00:00:00")
+              )
+              .slice(firstProduct, lastProduct)
+              .map((product) => {
+                return (
+                  <div className="shows__cards-box1">
+                    <img
+                      src={product.Photos[0].Path}
+                      alt="imagen show1"
+                      className="shows__cards-show1"
+                    />
+                    <div className="shows__cards-textContainer">
+                      <h1 className="shows__cards-texth1">{product.name}</h1>
+                      <h2 className="shows__cards-texth2">
+                        {formattedDate(product.StartDate).replace(/^\w/, (c) =>
+                          c.toUpperCase()
+                        )}
+                      </h2>
+                      <h3 className="shows__cards-texth3">
+                        {product.StartTime.slice(0, 2)} Horas
+                      </h3>
+                      <Link
+                        to={`product/${product.id}`}
+                        className="shows_cards-linkInfo"
+                      >
+                        +
+                      </Link>
+                    </div>
+                  </div>
+                );
+              })
+          )}
+        </div>
+      ) : (
+        <h1 className="shows__cards-h1FilterError">
+          No se encontraron shows con el fitro seleccionado
+        </h1>
+      )}
       {/* TRABAJAR FILTRADO POR CATEGORIAS */}
 
-      <div className="shows__categories-title">
-        <h4>CATEGORÍAS</h4>
-      </div>
-      <div className="shows__categories-box">
-        <p>Rock (2)</p>
-        <p>Clásico (1)</p>
-        <p>Jazz (3)</p>
-        <p>Pop (5)</p>
-        <p>Electrónica (2)</p>
-      </div>
+      {products.length ? (
+        <>
+          <div className="shows__categories-title">
+            <h4>
+              {selectedCategoryId !== "" ? (
+                <button
+                  onClick={() => {
+                    handleCategoryChange("");
+                  }}
+                >
+                  QUITAR FILTRO (X)
+                </button>
+              ) : (
+                "CATEGORÍAS"
+              )}
+            </h4>
+          </div>
+          <div className="shows__categories-box">
+            {uniqueCategories.map((c) => {
+              return (
+                <button
+                  onClick={() => {
+                    handleCategoryChange(c.id);
+                  }}
+                  className="shows__categories-buttons"
+                >
+                  {c.name} ({countCategories(products, c.name)})
+                </button>
+              );
+            })}
+          </div>
+        </>
+      ) : (
+        ""
+      )}
 
       {/* FIN FILTRADO POR CATEGORIAS */}
-      <Pagination
-        products={products}
-        handlePrev={handlePrev}
-        handleNext={handleNext}
-        currentPage={currentPage}
-      />
+      {products.length ? (
+        <Pagination
+          products={products}
+          handlePrev={handlePrev}
+          handleNext={handleNext}
+          currentPage={currentPage}
+        />
+      ) : (
+        ""
+      )}
     </div>
   );
 };
--- client/src/redux/reducer.js
+++ client/src/redux/reducer.js
@@ -3,7 +3,7 @@ import {
   SEARCH,
   GET_PRODUCTS,
   GET_PRODUCT_BY_ID,
-  FILTER_PRODUCTS_BY_DATE,
+  FILTERED_PRODUCTS,
 } from "./actions";
 
 const initialState = {
@@ -41,43 +41,13 @@ const rootReducer = (state = initialState, action) => {
         ...state,
         products: action.payload,
       };
-    case FILTER_PRODUCTS_BY_DATE:
-      if (action.payload === 1) {
-        return {
-          ...state,
-          products: state.filteredProducts.filter((event) => {
-            let eventDate = new Date(event.StartDate);
-            let currentDate = new Date();
-            let difference = (eventDate - currentDate) / (1000 * 60 * 60 * 24);
-            return difference <= 1;
-          }),
-        };
-      } else if (action.payload === 7) {
-        return {
-          ...state,
-          products: state.filteredProducts.filter((event) => {
-            let eventDate = new Date(event.StartDate);
-            let currentDate = new Date();
-            let difference = (eventDate - currentDate) / (1000 * 60 * 60 * 24);
-            return difference <= 7;
-          }),
-        };
-      } else if (action.payload === 15) {
-        return {
-          ...state,
-          products: state.filteredProducts.filter((event) => {
-            let eventDate = new Date(event.StartDate);
-            let currentDate = new Date();
-            let difference = (eventDate - currentDate) / (1000 * 60 * 60 * 24);
-            return difference <= 15;
-          }),
-        };
-      } else {
-        return {
-          ...state,
-          products: state.filteredProducts,
-        };
-      }
+    case FILTERED_PRODUCTS:
+      return {
+        ...state,
+        products: action.payload,
+        allProducts: action.payload,
+        filteredProducts: action.payload,
+      };
     default:
       return { ...state };
   }
