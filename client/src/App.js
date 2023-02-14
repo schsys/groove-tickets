486d0cfda974b0dab8e4e6eb2993019bc9e18012
soyfriss <federico.rissone@gmail.com>
2023-02-14 10:12:49 -0300

    feat(frontend/*): add 404 route validation

--- client/src/App.js
+++ client/src/App.js
@@ -1,65 +1,59 @@
 import React from "react";
-import { Route, useLocation } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Shows from "./components/Shows/Shows";
 import ProductDetails from "./components/ProductDetails/ProductDetails";
 import NavBar from "./components/NavBar/NavBar";
-// import LandingPage from "./components/LandingPage/LandingPage";
-// import CartSummary from "./components/CartSummary/CartSummary";
 import Footer from "./components/Footer/Footer";
 import Register from "./components/Register/Register";
 import Account from "./components/Account/Account";
 import Cart from "../src/components/Cart/Cart.jsx"
 import { AuthContextProvider } from "./context/AuthContext";
-import "./App.css";
 import { Order } from "./components/Order/Order";
 import AdminApp from "./admin";
+import Error404 from "./components/404/Error404";
+import "./App.css";
 
 function App() {
-  // const { pathname } = useLocation();
   return (
     <>
-      {/* <Route exact path={"/"} component={LandingPage} />
-      {pathname !== "/" ? <NavBar /> : null} */}
-      {/* <Route exact path={"/"} component={LandingPage} />
-      {pathname !== "/" ? <NavBar /> : null} */}
       <AuthContextProvider>
-        <Route exact path={"/"}>
-          <NavBar />
-          <Shows />
-          <Cart />
-        </Route>
-        <Route path="/product/:id">
-          <NavBar />
-          <ProductDetails />
-          <Footer />
-          <Cart />
-        </Route>
-        {/* <Route exact path={"/carrito"}>
-          <NavBar />
-          <CartSummary />
-        </Route> */}
-        <Route exact path={"/register"}>
-          <NavBar />
-          <Register />
-          <Footer />
-          <Cart />
-        </Route>
-        <Route exact path={"/micuenta"}>
-          <NavBar />
-          <Account />
-          <Footer />
-          <Cart />
-        </Route>
-        <Route exact path={"/comprar"}>
-          <NavBar isCartDisabled={true} />
-          <Order />
-          <Footer />
-        </Route>
-        <Route exact path={"/admin"}>
-          <AdminApp />
-        </Route>
-        {/* <Cart /> */}
-        {/* {pathname !== "/" ? <Footer /> : null} */}
+        <Switch>
+          <Route exact path={"/"}>
+            <NavBar />
+            <Shows />
+            <Footer />
+            <Cart />
+          </Route>
+          <Route path="/product/:id">
+            <NavBar />
+            <ProductDetails />
+            <Footer />
+            <Cart />
+          </Route>
+          <Route exact path={"/register"}>
+            <NavBar />
+            <Register />
+            <Footer />
+            <Cart />
+          </Route>
+          <Route exact path={"/micuenta"}>
+            <NavBar />
+            <Account />
+            <Footer />
+            <Cart />
+          </Route>
+          <Route exact path={"/comprar"}>
+            <NavBar isCartDisabled={true} />
+            <Order />
+            <Footer />
+          </Route>
+          <Route exact path={"/admin"}>
+            <AdminApp />
+          </Route>
+          <Route path='*'>
+            <Error404 />
+          </Route>
+        </Switch>
       </AuthContextProvider>
     </>
   );
---
+++ client/src/components/404/Error404.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Error404() {
+    return <>
+        <p>Ruta inválida!</p>
+    </>
+}
