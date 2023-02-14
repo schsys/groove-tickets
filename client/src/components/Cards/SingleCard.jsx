4a4823148347872580234af98c3bf5b5fd841d42
nicobonder <nicobonder@hotmail.com>
2023-02-14 08:42:31 -0300

    Hover added. Resized cards

--- client/src/components/Cards/SingleCard.jsx
+++ client/src/components/Cards/SingleCard.jsx
@@ -16,25 +16,27 @@ const SingleCard = (data) => {
 
   return (
     <div className="shows__cards-box1" key={data.data.id}>
-      <img
-        src={data.data.Photos[0].Path}
-        alt="imagen show1"
-        className="shows__cards-show1"
-      />
-      <div className="shows__cards-textContainer">
-        <h1 className="shows__cards-texth1">{data.data.name}</h1>
-        <h2 className="shows__cards-texth2">
-          {formattedDate(data.data.StartDate).replace(/^\w/, (c) =>
-            c.toUpperCase()
-          )}
-        </h2>
-        <h3 className="shows__cards-texth3">
-          {data.data.StartTime.slice(0, 2)} Horas
-        </h3>
-        <Link to={`product/${data.data.id}`} className="shows_cards-linkInfo">
-          <FaInfoCircle />
-        </Link>
-      </div>
+      <Link className="shows__cards-link" to={`product/${data.data.id}`}>
+        <img
+          src={data.data.Photos[0].Path}
+          alt="imagen show1"
+          className="shows__cards-show1"
+        />
+        <div className="shows__cards-textContainer">
+          <h1 className="shows__cards-texth1">{data.data.name}</h1>
+          <h2 className="shows__cards-texth2">
+            {formattedDate(data.data.StartDate).replace(/^\w/, (c) =>
+              c.toUpperCase()
+            )}
+          </h2>
+          <h3 className="shows__cards-texth3">
+            {data.data.StartTime.slice(0, 2)} Horas
+          </h3>
+          <Link to={`product/${data.data.id}`} className="shows_cards-linkInfo">
+            <FaInfoCircle />
+          </Link>
+        </div>
+      </Link>
     </div>
   );
 };
