78408c7ab3856c0afb66a8d36dbebe3a93f20f48
nicobonder <nicobonder@hotmail.com>
2023-02-14 10:22:52 -0300

    3D Hover in card is working. Needs improvment

--- client/src/components/Cards/SingleCard.jsx
+++ client/src/components/Cards/SingleCard.jsx
@@ -1,21 +1,43 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import { formattedDate } from "../utils/formatedDate";
 import { FaInfoCircle } from "react-icons/fa";
 import "../Shows/Shows.css";
 
 const SingleCard = (data) => {
-  // console.log("-- inicio de datos --");
-  // console.log('la data completa: ', data)
-  // console.log('id: ', data.data.id)
-  // console.log('imagen: ', data.data.Photos[0].Path)
-  // console.log('nombre: ', data.data.name)
-  // console.log('fecha: ', data.data.StartDate)
-  // console.log('hora: ', data.data.StartTime)
-  // console.log("-- fin de datos --");
+  const cardRef = useRef(null);
+
+  useEffect(() => {
+    const card = cardRef.current;
+    const THRESHOLD = 15;
+
+    function handleHover(e) {
+      const { clientX, clientY, currentTarget } = e;
+      const { clientWidth, clientHeight, offsetLeft, offsetTop } = currentTarget;
+
+      const horizontal = (clientX - offsetLeft) / clientWidth;
+      const vertical = (clientY - offsetTop) / clientHeight;
+      const rotateX = (THRESHOLD / 2 - horizontal * THRESHOLD).toFixed(2);
+      const rotateY = (vertical * THRESHOLD - THRESHOLD / 2).toFixed(2);
+      card.style.transform = `perspective(${clientWidth}px) rotateX(${rotateY}deg) rotateY(${rotateX}deg) scale3d(1, 1, 1)`;
+    }
+
+    function resetStyles(e) {
+      card.style.transform = `perspective(${e.currentTarget.clientWidth}px) rotateX(0deg) rotateY(0deg)`;
+    }
+
+    card.addEventListener("mousemove", handleHover);
+    card.addEventListener("mouseleave", resetStyles);
+
+    return () => {
+      card.removeEventListener("mousemove", handleHover);
+      card.removeEventListener("mouseleave", resetStyles);
+    };
+  }, [cardRef]);
+
 
   return (
-    <div className="shows__cards-box1" key={data.data.id}>
+    <div className="shows__cards-box1" ref={cardRef} key={data.data.id}>
       <Link className="shows__cards-link" to={`product/${data.data.id}`}>
         <img
           src={data.data.Photos[0].Path}
@@ -42,3 +64,30 @@ const SingleCard = (data) => {
 };
 
 export default SingleCard;
+
+
+
+  /*Codigo para dar efecto 3D*/
+  // {
+  //   const card = document.querySelector(".shows__cards-box1");
+  //   const THRESHOLD = 15;
+   
+  //  function handleHover(e) {
+  //    const { clientX, clientY, currentTarget } = e;
+  //    const { clientWidth, clientHeight, offsetLeft, offsetTop } = currentTarget;
+   
+  //    const horizontal = (clientX - offsetLeft) / clientWidth;
+  //    const vertical = (clientY - offsetTop) / clientHeight;
+  //    const rotateX = (THRESHOLD / 2 - horizontal * THRESHOLD).toFixed(2);
+  //    const rotateY = (vertical * THRESHOLD - THRESHOLD / 2).toFixed(2);
+  //    card.style.transform =
+  //      `perspective(${clientWidth}px) rotateX(${rotateY}deg) rotateY(${rotateX}deg) scale3d(1, 1, 1)`;
+  //  }
+   
+  //  function resetStyles(e) {
+  //    card.style.transform =
+  //      `perspective(${e.currentTarget.clientWidth}px) rotateX(0deg) rotateY(0deg)`;
+  //  }
+  //  card.addEventListener("mousemove", handleHover);
+  //  card.addEventListener("mouseleave", resetStyles);
+  // } 
