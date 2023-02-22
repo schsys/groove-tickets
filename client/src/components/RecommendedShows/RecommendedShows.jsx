a0278770d05078e6e55e71c1e57fa0ca8e26f1a6
soyfriss <federico.rissone@gmail.com>
2023-02-22 20:27:30 -0300

    refactor(shows): filter recommended shows (#122)

--- client/src/components/RecommendedShows/RecommendedShows.jsx
+++ client/src/components/RecommendedShows/RecommendedShows.jsx
@@ -1,33 +1,43 @@
 import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux';
 
-export default function RecommendedShows({categories}) {
+export default function RecommendedShows({ referencedShowId, categories }) {
     const products = useSelector((state) => state.products);
     const [productByCat, setProductByCat] = useState([])
 
-
     useEffect(() => {
-        console.log('categories', categories)
-        console.log('products', products)
-        setProductByCat(products.filter((p) => p.categories.filter((c) => categories.includes(c.Id)).length > 0
-        ))
-    }, [categories, products])
+        // console.log('categories', categories)
+        // console.log('products', products)
+        const filteredProducts = products.filter((p) => {
+            console.log('product: ', p); 
+            
+            if (p.id === referencedShowId) return false;
+
+            return p.Categories.filter((c) => {
+                const isCategoryIncluded = categories.includes(c.id)
+                console.log('category: ', c.id, isCategoryIncluded);
+
+                return isCategoryIncluded;
+            }).length > 0
+        })
+        setProductByCat(filteredProducts);
+    }, [referencedShowId, categories, products])
 
     console.log('products', products)
-   
-  return (
-    <div className='recommended_section'>
-        {productByCat.map((p) => {
-            return(
-                <div>
-                    <h2>p.name</h2>
-                    <h2>Titulo o algo</h2>
-                </div>
-            )
-        })}
-       
-     
-        <p>HOla categories</p>
-    </div>
-  )
+
+    return (
+        <div className='recommended_section'>
+            {console.log('productByCat: ', productByCat)}
+            {productByCat.map((p) => {
+                return (
+                    <div>
+                        <h2>{p.name}</h2>
+                    </div>
+                )
+            })}
+
+
+            <p>HOla categories</p>
+        </div>
+    )
 }
