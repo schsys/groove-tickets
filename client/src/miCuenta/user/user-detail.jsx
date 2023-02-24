7028953817364ffedffbe4f6f2ccc6479ecf5dff
JIM90mdp <juanignaciomascarenhas@gmail.com>
2023-02-24 00:40:52 -0300

    refactor: user&customerDetails

--- client/src/miCuenta/user/user-detail.jsx
+++ client/src/miCuenta/user/user-detail.jsx
@@ -2,6 +2,7 @@ import {
   DateField,
   EmailField,
   NumberField,
+  PasswordInput,
   Show,
   SimpleShowLayout,
   TextField,
@@ -11,6 +12,7 @@ import { UserAuth } from "../../context/AuthContext";
 import React, { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import { getDetailedUser } from "../../common/integrations/api";
+import { Password } from "@mui/icons-material";
 
 export const UserDetail = () => {
   const { user } = UserAuth();
@@ -54,7 +56,7 @@ export const UserDetail = () => {
   console.log("apiUser.item.customer: ", apiUser.item.Customer);
   // {
   //   apiUser.item.Customer ? (
-  //   // SI hubiera customer, habría que despachar un get de customer, no de user. 
+  //   // SI hubiera customer, habría que despachar un get de customer, no de user.
   //     <Show title="User Detail" id={apiUser.item.Customer.id}>
   //       <SimpleShowLayout>
   //         <TextField source="name" label="Nombre" />
@@ -86,22 +88,21 @@ export const UserDetail = () => {
         <TextField source="userName" />
         <TextField source="role" />
         <TextField source="status" />
+        {/* <PasswordInput source="password" /> */}
       </SimpleShowLayout>
-   {apiUser.item.Customer && (
-        <SimpleShowLayout>
-          {/* Como acceder a las propiedades de Customer? */}
-          <TextField source="name" label="Nombre" />
-          <TextField source="address" label="Dirección" />
-          <TextField source="city" label="Ciudad" />
-          <TextField source="state" label="Provincia" />
-          <NumberField source="zip" label="Código Postal" />
-          <EmailField source="email" label="Email" />
-          <TextField source="telephone" label="Teléfono" />
-          <TextField source="document" label="Documento" />
-          <DateField source="birthDate" label="Fecha de nacimiento" />
-          <TextField source="status" label="Estado" />
-        </SimpleShowLayout>
-      )}
+
+      <SimpleShowLayout source="Customer">
+        <TextField source="Customer.name" label="Nombre" />
+        <TextField source="Customer.address" label="Dirección" />
+        <TextField source="Customer.city" label="Ciudad" />
+        <TextField source="Customer.state" label="Provincia" />
+        <NumberField source="Customer.zip" label="Código Postal" />
+        <EmailField source="Customer.email" label="Email" />
+        <TextField source="Customer.telephone" label="Teléfono" />
+        <TextField source="Customer.document" label="Documento" />
+        <DateField source="Customer.birthDate" label="Fecha de nacimiento" />
+        <TextField source="Customer.status" label="Estado" />
+      </SimpleShowLayout>
     </Show>
   );
 };
