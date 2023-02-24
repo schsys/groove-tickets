e8e12a00a269f88c8d7fdaad3359a4b8101eebe7
goya0310 <108840268+goya0310@users.noreply.github.com>
2023-02-24 01:26:20 -0300

    Update ItemsOrder.jsx

--- client/src/components/Order/ItemsOrder.jsx
+++ client/src/components/Order/ItemsOrder.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-// import { Link } from "react-router-dom";
 import moment from "moment-timezone";
 // import { getTotalItems } from "../../redux/actions";
 // import { useDispatch, useSelector } from "react-redux";
