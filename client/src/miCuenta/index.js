6e1fc7730f787d24d4ae23232430806c2c17319f
JIM90mdp <juanignaciomascarenhas@gmail.com>
2023-02-25 17:01:47 -0300

    userReview: data provider autentication(chequear), review mangament(noFeat)

--- client/src/miCuenta/index.js
+++ client/src/miCuenta/index.js
@@ -3,6 +3,10 @@ import { Admin, Resource } from 'react-admin';
 import { UserDetail } from './user/user-detail';
 import { UserEdit } from './user/user-edit';
 
+import { ReviewList } from './review/review-list';
+import { ReviewShow } from './review/review-show';
+import { ReviewCreate } from './review/review-create';
+
 import { Dashboard } from './dashboard/dashboard';
 import { dataProvider } from './data-provider';
 
@@ -23,6 +27,13 @@ const App = () => (
             name="customers"
             list={CustomerDetail}
             edit={CustomerEdit}
+        />
+        <Resource
+            name="reviews"
+            list={ReviewList}
+            create={ReviewCreate}
+            show={ReviewShow}
+            recordRepresentation="stars"
         />
     </Admin>
 );
