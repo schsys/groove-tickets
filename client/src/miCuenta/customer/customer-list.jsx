f0db7454f01a07f96d96df9df546bcda744f8ce0
soyfriss <federico.rissone@gmail.com>
2023-03-02 13:38:55 -0300

    feat(micuenta): new menu entries

--- client/src/miCuenta/customer/customer-list.jsx
+++ client/src/miCuenta/customer/customer-list.jsx
@@ -63,11 +63,11 @@ export const CustomerList = () => {
       pagination={false}
     >
       <Datagrid bulkActionButtons={false} size="medium">
-        <TextField source="telephone" label="Teléfono" />
-        <TextField source="document" label="Documento" />
-        <TextField source="address" label="Dirección" />
-        <TextField source="city" label="Ciudad" />
-        <DateField source="birthDate" label="Fecha de Nacimiento" />
+        <TextField source="telephone" label="Teléfono" sortable={false} />
+        <TextField source="document" label="Documento" sortable={false} />
+        <TextField source="address" label="Dirección" sortable={false} />
+        <TextField source="city" label="Ciudad" sortable={false} />
+        <DateField source="birthDate" label="Fecha de Nacimiento" sortable={false} />
         <EditButton />
       </Datagrid>
     </List>
--- client/src/miCuenta/layout/menu.js
+++ client/src/miCuenta/layout/menu.js
@@ -3,8 +3,26 @@ import { MenuItemLink } from "react-admin";
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import ReceiptIcon from '@mui/icons-material/Receipt';
 import ReviewsIcon from '@mui/icons-material/Reviews';
+import LogoutIcon from '@mui/icons-material/Logout';
+import HomeIcon from '@mui/icons-material/Home';
+import Swal from "sweetalert2";
+import { UserAuth } from "../../context/AuthContext";
+import { useHistory } from "react-router-dom";
 
 export const Menu = () => {
+  const history = useHistory();
+  const { logout } = UserAuth();
+
+  const handleLogout = async () => {
+    await logout();
+
+    Swal.fire({
+      title: "Yazz",
+      html: "<h3>Gracias, te esperamos la próxima</h3>",
+    });
+    history.push("/");
+  };
+
   return (
     <div>
       <MenuItemLink
@@ -22,6 +40,8 @@ export const Menu = () => {
         primaryText="Reviews"
         leftIcon={<ReviewsIcon />}
       />
+      <MenuItemLink to="" onClick={() => history.push("/")} primaryText="Back to site" leftIcon={<HomeIcon />} />
+      <MenuItemLink to="" onClick={handleLogout} primaryText="Logout" leftIcon={<LogoutIcon />} />
     </div>
   );
 };
