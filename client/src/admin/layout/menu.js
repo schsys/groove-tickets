792941dafa0e3170bb41e5b462be9ae5bedfaa82
soyfriss <federico.rissone@gmail.com>
2023-03-02 13:39:19 -0300

    feat(admin): new menu entries

--- client/src/admin/layout/menu.js
+++ client/src/admin/layout/menu.js
@@ -1,19 +1,34 @@
 import * as React from "react";
 import { DashboardMenuItem, MenuItemLink } from "react-admin";
-import { Button } from "@mui/material"
-import BookIcon from "@mui/icons-material/Book";
-// import ChatBubbleIcon from "@mui/icons-material/ChatBubble";
+import { useHistory } from "react-router-dom";
+import ReviewsIcon from '@mui/icons-material/Reviews';
 import PeopleIcon from "@mui/icons-material/People";
-// import LabelIcon from "@mui/icons-material/Label";
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 import GroupsIcon from '@mui/icons-material/Groups';
 import CategoryIcon from '@mui/icons-material/Category';
 import SlideshowIcon from '@mui/icons-material/Slideshow';
 import WifiCalling3Icon from '@mui/icons-material/WifiCalling3';
-import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
+import ReceiptIcon from '@mui/icons-material/Receipt';
 import NotificationsIcon from '@mui/icons-material/Notifications';
+import LogoutIcon from '@mui/icons-material/Logout';
+import HomeIcon from '@mui/icons-material/Home';
+import Swal from "sweetalert2";
+import { UserAuth } from "../../context/AuthContext";
 
 export const Menu = () => {
+  const history = useHistory();
+  const { logout } = UserAuth();
+
+  const handleLogout = async () => {
+    await logout();
+
+    Swal.fire({
+      title: "Yazz",
+      html: "<h3>Gracias, te esperamos la próxima</h3>",
+    });
+    history.push("/");
+  };
+
   return (
     <div>
       <DashboardMenuItem />
@@ -27,15 +42,15 @@ export const Menu = () => {
         leftIcon={<PeopleIcon />}
       />
       <MenuItemLink to="/customers" primaryText="Customer" leftIcon={<WifiCalling3Icon />} />
-      <MenuItemLink to="/orders" primaryText="Order" leftIcon={<BookmarkBorderIcon />} />
+      <MenuItemLink to="/orders" primaryText="Order" leftIcon={<ReceiptIcon />} />
       <MenuItemLink
         to="/reviews"
         primaryText="Review"
-        leftIcon={<BookIcon />}
+        leftIcon={<ReviewsIcon />}
       />
       <MenuItemLink to="/mailgen" primaryText="Notification" leftIcon={<NotificationsIcon />} />
+      <MenuItemLink to="" onClick={() => history.push("/")} primaryText="Back to site" leftIcon={<HomeIcon />} />
+      <MenuItemLink to="" onClick={handleLogout} primaryText="Logout" leftIcon={<LogoutIcon />} />
     </div>
   );
 }
-
-
