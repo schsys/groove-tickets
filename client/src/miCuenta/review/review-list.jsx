29e89084b36d7c146a93db50510f903b765c1323
soyfriss <federico.rissone@gmail.com>
2023-03-02 21:34:24 -0300

    feat(micuenta): data-provider auth headers (#202)

--- client/src/miCuenta/review/review-list.jsx
+++ client/src/miCuenta/review/review-list.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import {
   Datagrid,
   List,
@@ -8,11 +9,7 @@ import {
   RichTextField,
 } from "react-admin";
 import StarRatingField from "./star-rating-field";
-import React, { useState, useEffect } from "react";
-import { getDetailedUser } from "../../common/integrations/api";
-import { NavLink } from "react-router-dom";
-import { UserAuth } from "../../context/AuthContext";
-import { Card, CardContent, CardHeader } from "@mui/material";
+import { PrivateContent } from "../../components/Authentication/PrivateContent";
 
 const ReviewShow = () => (
   <SimpleShowLayout>
@@ -20,79 +17,38 @@ const ReviewShow = () => (
   </SimpleShowLayout>
 )
 
-export const ReviewList = () => {
-  const { user } = UserAuth();
-
-  const [apiUser, setApiUser] = useState({
-    item: {},
-    fetchStatus: "loading",
-    error: null,
-  });
-
-  useEffect(() => {
-    console.log("Account useEffect()");
-
-    async function getApiUser(username) {
-      const response = await getDetailedUser(username);
-      console.log("response, user: ", response);
-      setApiUser(response);
-    }
-
-    getApiUser(user.email);
-  }, [user]);
-
-  if (apiUser.fetchStatus === "loading") {
-    return <>
-      <Card sx={{ mt: 1 }}>
-        <CardContent>
-          <p>Obteniendo datos...</p>
-        </CardContent>
-      </Card>
-    </>
-  }
-
-  if (apiUser.fetchStatus === "failed") {
-    return <>
-      <Card sx={{ mt: 1 }}>
-        <CardHeader title="Oops! Esto es embarazoso!" />
-        <CardContent>
-          <p>{apiUser.error && apiUser.error.message}</p>
-          <NavLink to="/">Volver</NavLink>
-        </CardContent>
-      </Card>
-    </>
-  }
-
-  return <>
-    <List
-      sort={{ field: "createdAt", order: "DESC" }}
-      queryOptions={{ meta: { resource: `${apiUser.item.id}/reviews` } }}
-      title="Reviews"
-      exporter={false}
-    >
-      <Datagrid
-        bulkActionButtons={false}
-        expand={<ReviewShow />}
-        size="medium"
+export const ReviewList = () =>
+  <PrivateContent>
+    {props =>
+      <List
+        sort={{ field: "createdAt", order: "DESC" }}
+        queryOptions={{ meta: { resource: `${props.userId}/reviews` } }}
+        title="Reviews"
+        exporter={false}
       >
-        <StarRatingField size="small" label="Puntaje" />
-        <TextField source="Product.name" label="Producto" />
-        <FunctionField
-          source="message"
-          label="Opinión"
-          render={(record) => {
-            if (record.message && record.message.length <= 20) {
-              return record.message;
-            }
-            if (record.message) {
-              return record.message.slice(0, 30) + "...";
-            }
-            return "";
-          }}
-        />
-        <DateField source="createdAt" label="Fecha de Creación" showTime />
-        <DateField source="updatedAt" label="Fecha de Actualización" showTime />
-      </Datagrid>
-    </List>
-  </>
-};
+        <Datagrid
+          bulkActionButtons={false}
+          expand={<ReviewShow />}
+          size="medium"
+        >
+          <StarRatingField size="small" label="Puntaje" />
+          <TextField source="Product.name" label="Producto" />
+          <FunctionField
+            source="message"
+            label="Opinión"
+            render={(record) => {
+              if (record.message && record.message.length <= 20) {
+                return record.message;
+              }
+              if (record.message) {
+                return record.message.slice(0, 30) + "...";
+              }
+              return "";
+            }}
+          />
+          <DateField source="createdAt" label="Fecha de Creación" showTime />
+          <DateField source="updatedAt" label="Fecha de Actualización" showTime />
+        </Datagrid>
+      </List>
+    }
+  </PrivateContent>
